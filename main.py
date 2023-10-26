8035115abfd9d3f756be076135495d458be534e0
Pavel Vasiliev <vasilievpavel4@gmail.com>
2023-10-26 11:35:14 +0500

    хопы в днс имени и дебаг режим

--- main.py
+++ main.py
@@ -11,15 +11,17 @@ if __name__ == '__main__':
     parser.add_argument('-pkg', '--package_size', default=40, type=int, help='Sets a packet size')
     parser.add_argument('-try', '--retry', type=int, default=20,
                         help='Sets the maximum number of attempts to send a packet and obtain information about the end node')
+    parser.add_argument('-debug', '--debug', type=bool, default=False,
+                        help='Sets the maximum number of attempts to send a packet and obtain information about the end node')
     parser.add_argument('IP_ADDRESS', help='Sets the target IP address')
 
 
     args = parser.parse_args()
     # args = parser.parse_args("python main.py -p 80 -t 1 176.100.119.169".split())
-    print(args)
+    # print(args)
     if not vars(args):
         parser.print_usage()
     else:
         host = socket.gethostbyname(args.IP_ADDRESS)
-        traceroute = TraceRouter(host, args.port, args.seq, args.timeout, args.retry, args.ttl)
+        traceroute = TraceRouter(host, args.port, args.seq, args.timeout, args.retry, args.ttl, args.debug)
         traceroute.get_trace()
--- trace_router.py
+++ trace_router.py
@@ -5,9 +5,11 @@ from scapy.layers.inet import ICMP
 from scapy.layers.inet import IP
 from scapy.layers.inet6 import IPv6
 
+import ipwhois
+
 
 class TraceRouter:
-    def __init__(self, ip_address, port=80, seq=1, timeout=0.5, retry_count=3, max_ttl=20, packet_size=40):
+    def __init__(self, ip_address, port=80, seq=1, timeout=0.5, retry_count=3, max_ttl=20, packet_size=40, debug=True):
         self.ip = ip_address
         self.port = port
         self.timeout = timeout
@@ -16,18 +18,24 @@ class TraceRouter:
         self.seq = seq
         self.data = str([str(i) for i in range(packet_size)]).encode()
         self.ip_addresses = []
+        self.debug = debug
 
     def get_trace(self):
-        self.print_request_status('Node number', 'Time', 'IP-address', 'Status')
+        if not self.debug:
+            self.print_request_status('Node number', 'Time', 'IP-address', 'Status')
         ip_version = ipaddress.ip_address(self.ip).version
         protocol_packet = ICMP(seq=self.seq)
 
         for ttl in range(1, self.max_ttl + 1):
+            if self.debug:
+                print(f'The packet with ttl = {ttl} was sent to the address {self.get_name_by_host(self.ip)}')
             reply, packet = self.get_node_info_with_retry(ttl, ip_version, protocol_packet)
 
             if reply is None:
                 self.print_request_status(ttl, '*', '*', 'Request timeout exceeded.')
             else:
+                if self.debug:
+                    print(f'The packet delivered to the address {self.get_name_by_host(self.ip)} successfully')
                 src = reply.src
                 seq = reply[ICMP].seq
                 self.seq = seq
@@ -36,6 +44,7 @@ class TraceRouter:
                 self.ip_addresses += [src]
                 if src == self.ip:
                     break
+
 
 
     def get_node_info_with_retry(self, ttl, ip_version, protocol_packet):
@@ -53,6 +62,18 @@ class TraceRouter:
 
         return ip_packet
 
-    @staticmethod
-    def print_request_status(number, ping, ip_addr, status):
-        print(f'{str(number).ljust(15)} {ping.ljust(15)}  {ip_addr.rjust(18)}      {status}')
+    def print_request_status(self, number, ping, ip_addr, status):
+        try:
+            host_name = self.get_name_by_host(ip_addr)
+        except:
+            host_name = ''
+
+        if self.debug:
+            print(f'The packet was sent within time {ping} to the address {ip_addr} ({host_name}) with status \"{status}\"')
+            return
+
+        print(f'{status} {str(number).ljust(15)} {ping.ljust(15)}  {ip_addr.rjust(18)} {host_name.ljust(20)} ')
+
+    def get_name_by_host(self, ip_addr):
+        host_name = ipwhois.IPWhois(ip_addr).lookup_whois()['nets'][0]['name']
+        return f'{ip_addr} ({host_name})'
